97367dc94fec35cdd6fff53ad9325624e5781298
bartoszukm <bartoszuk@rexamine.com>
2015-03-14 14:31:35 +0100

    Added print and format features in stack and queue class

---
+++ R/queue.R
@@ -0,0 +1,9 @@
+print.queue <- function(queue)
+{
+   print(as.list.Queue(queue))
+}
+
+format.queue <- function(queue, ...)
+{
+   format(as.list.Queue(queue), ...)
+}
---
+++ R/stack.R
@@ -0,0 +1,9 @@
+print.stack <- function(stack)
+{
+   print(as.list.Stack(stack))
+}
+
+format.stack <- function(stack, ...)
+{
+   format(as.list.Stack(stack), ...)
+}
